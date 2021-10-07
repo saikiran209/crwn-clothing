f3a833218c892862eca06dd409d3c5d75abe1855
saikiran209 <amudala.saikiran@gmail.com>
2021-10-06 20:17:06 -0500

    Added Routes to the application for Hats

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { HomePage } from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 
 function App() {
   return (
     <div>
+      <Switch>
+         <Route exact path='/' component={HomePage} />
+         <Route exact path='/hats' component={HatsPage} />
+         
+      </Switch>
          <HomePage />       
     </div>
   );
