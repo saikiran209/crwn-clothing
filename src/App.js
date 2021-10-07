e988c930a56bd30e4a6e5743820cbcec6de987db
saikiran209 <amudala.saikiran@gmail.com>
2021-10-07 14:02:14 -0500

    Added the Shop Page and CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import { HomePage } from './pages/homepage/homepage.component';
+import  { HomePage }  from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
 
 
 function App() {
@@ -16,10 +10,10 @@ function App() {
     <div>
       <Switch>
          <Route exact path='/' component={HomePage} />
-         <Route exact path='/hats' component={HatsPage} />
+         <Route exact path='/shop' component={ShopPage} />
          
       </Switch>
-         <HomePage />       
+         
     </div>
   );
 }
